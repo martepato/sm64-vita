1893edf7bff9f33402327adf38b0bd937f1483dc
bythos14 <45720552+bythos14@users.noreply.github.com>
2021-02-25 12:19:19 -0400

    Remove some dependencies from build_deps.sh
    These are included in VitaSDK and no longer need to be manually built.

--- build_deps.sh
+++ build_deps.sh
@@ -9,22 +9,6 @@ git clone https://github.com/Rinnegatamante/vitaGL.git
 cd vitaGL
 git reset --hard 9c963ab
 make HAVE_SBRK=1 HAVE_SHARK=1 install -j4
-cd ../
-
-git clone https://github.com/Rinnegatamante/math-neon.git
-cd math-neon
-make install -j4
-cd ../
-
-git clone https://github.com/Rinnegatamante/vitaShaRK.git
-cd vitaShaRK
-# Taken from https://github.com/Rinnegatamante/vitaShaRK/README.md
-vita-libs-gen SceShaccCg.yml build
-cd build
-make install
-cd ..
-cp shacccg.h $VITASDK/arm-vita-eabi/include/psp2/shacccg.h
-make install
 
 cd ../../
 rm -rf deps
