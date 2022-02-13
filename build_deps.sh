9308f1cb0350cd7a8bc67ade9656cf49b49eff97
nor2101 <61382188+nor2101@users.noreply.github.com>
2022-02-13 16:18:02 +0100

    build_deps.sh: fix building old libs
    Build old versions of VitaGL and VitaShaRK to fix compiling with newer VitaSDK versions

--- build_deps.sh
+++ build_deps.sh
@@ -3,12 +3,19 @@
 mkdir deps
 cd deps
 
-# Build and install vitaGL, mathneon and vitaShaRK
+# Build and install old versions of vitaGL and vitaShaRK
 
 git clone https://github.com/Rinnegatamante/vitaGL.git
 cd vitaGL
 git reset --hard 7b9de85
-make HAVE_SBRK=1 HAVE_SHARK=1 install -j4
+make HAVE_SBRK=1 HAVE_SHARK=1 install -j$(nproc)
+cd ..
 
-cd ../../
+git clone https://github.com/Rinnegatamante/vitaShaRK.git
+cd vitaShaRK
+git reset --hard 30e2361
+make install -j$(nproc)
+cd ..
+
+cd ..
 rm -rf deps
